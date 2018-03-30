479f59c34c5d72c5638f19c4317f11dceaf1585c
TomAwezome <sonidd1@yahoo.com>
2018-03-30 09:53:21 -0400

    beginning class overhaul

---
+++ veno/modules/file.py
@@ -0,0 +1,30 @@
+# def start(venicGlobals):
+# 	import argparse # venic is required to be started with a filename, need to parse the arg for it
+# 	parser = argparse.ArgumentParser()      # initialize argument parser
+# 	parser.add_argument("filename") # add an argument to our program, we need a file
+# 	args = parser.parse_args()      # actually parse the arguments Venic was given
+# 	file = open(args.filename)
+# 	fileString = file.read()
+# 	file.close()
+# 	venicGlobals["venicFile"] = fileString
+# 	venicGlobals["venicFileName"] = args.filename
+# 	try:
+# 		try:
+# 			venicGlobals["lexer"] = venicGlobals["pygments"].lexers.guess_lexer_for_filename(args.filename,fileString)
+# 		except:
+# 			venicGlobals["lexer"] = venicGlobals["pygments"].lexers.guess_lexer(fileString)
+# 		if venicGlobals["lexer"].name == "PHP":
+# 			venicGlobals["lexer"] = venicGlobals["pygments"].lexers.PhpLexer(startinline=True)
+# 	except:
+# 		venicGlobals["lexer"] = None
+# def loop(venicGlobals):
+# 	pass
+# def kill(venicGlobals):
+# 	pass
+
+class File:
+	def __init__(self, source):
+		self.source = source	# source should be a filename string, I'm assuming it'll end up being relative...
+		file = open(source);
+		self.contents = file.read()
+		file.close()
