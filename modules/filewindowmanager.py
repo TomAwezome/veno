4ddb3fcf938aa869384ab5aebc92fe281e464349
TomAwezome <sonidd1@yahoo.com>
2022-07-25 14:10:15 -0400

    Clean FileWindowManager code slightly.

--- modules/filewindowmanager.py
+++ modules/filewindowmanager.py
@@ -1,10 +1,3 @@
-"""
-this needs the logic for:
-- swap the file windows
-- open the files up at init and set internal variables
-- maintain current_file_window state 
-- terminate all the filewindows at shutdown
-"""
 class FileWindowManager:
 	def __init__(self, engine):
 		self.engine = engine
@@ -17,9 +10,7 @@ class FileWindowManager:
 
 		for filename in self.engine.filenames:
 			try:
-				file = File(filename)
-#				self.files.append(file)											# Load file provided as only arg.
-				file_window = FileWindow(self.engine, file)		# Create fileWindow.
+				file_window = FileWindow(self.engine, File(filename))		# Create fileWindow.
 				self.file_window_list.append(file_window)
 				file_window.update()											# Update fileWindow contents.
 			except IsADirectoryError:
@@ -28,9 +19,7 @@ class FileWindowManager:
 		if self.file_window_list != []:
 			self.engine.set("current_file_window", self.file_window_list[0])
 		else: # filewindow list can be empty if provided arg is a directory and no other filename args are given
-			file = File("untitled.txt")
-#			self.files.append(file)
-			file_window = FileWindow(self.engine, file)
+			file_window = FileWindow(self.engine, File("untitled.txt"))
 			self.file_window_list.append(file_window)
 			file_window.update()
 			self.engine.set("current_file_window", self.file_window_list[0])
@@ -40,4 +29,6 @@ class FileWindowManager:
 		pass
 
 	def terminate(self):
-		pass
+		for file_window in self.file_window_list:
+			file_window.terminate()
+
